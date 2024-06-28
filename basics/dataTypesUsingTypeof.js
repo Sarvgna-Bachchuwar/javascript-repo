0537936056668cb610ca589fea237018e6d14a35
Sarvgna Bachchuwar <59611284+Sarvgna-Bachchuwar@users.noreply.github.com>
2024-06-28 19:37:54 +0000

    memory allocation for datatypes

--- basics/dataTypesUsingTypeof.js
+++ basics/dataTypesUsingTypeof.js
@@ -35,16 +35,25 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // Return type of variables in JavaScript
-// 1) Primitive Datatypes
-//        Number => number
-//        String  => string
-//        Boolean  => boolean
-//        null  => object
-//        undefined  =>  undefined
-//        Symbol  =>  symbol
-//        BigInt  =>  bigint
+// 1. Primitive datatype:-
+//     1.String
+//     2. Number
+//     3. Boolean (true / false)
+//     4. null (iska matlab hai empty or zero nahi hai)
+//     5. undefined (aapne variable declare kardiya hai but usmein aap kya value dalenge aapne decide nahi kiya hai)
+//     6. Symbol (kisi bhi value ko unique banane ke liye use kiya jata hai)
+//     7. BigInt (jo badi values hoti hai oo hum BigInt mein use karte hai)
 
-// 2) Non-primitive Datatypes
-//        Arrays  =>  object
-//        Function  =>  function
-//        Object  =>  object
+// 2. Preference datatype (Non Primitive datatype)
+//     1. Array ( [ ] )
+//     2. Object ( { } )
+//     3. Function ( function( ){ } )
+
+// ------***--- 
+// Memory allocation
+// /datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+// JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
